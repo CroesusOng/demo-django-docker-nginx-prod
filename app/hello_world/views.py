37468a3f379d071b297df893e03057203a705712
cc <c@c.c>
2021-03-27 08:25:14 +0000

    beta

--- app/hello_world/views.py
+++ app/hello_world/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """Placeholder index view"""
     return render(request, 'index.html')
+
+def index(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+    })
